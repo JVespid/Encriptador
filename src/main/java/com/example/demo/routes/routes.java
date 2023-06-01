945f018a5ea07e611a5e91386599d1c881f961e1
JVespid <vespid.vj@gmail.com>
2023-06-01 11:16:58 -0600

    agregando servidor de spring - java - maven

---
+++ src/main/java/com/example/demo/routes/routes.java
@@ -0,0 +1,15 @@
+package com.example.demo.routes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class routes {
+	
+	@GetMapping(value="/")
+	public String index () {
+		
+		return "index";
+		
+	}
+}
